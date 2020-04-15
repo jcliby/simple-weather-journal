7df92a77537d994b31837999942a4afa3c1b5e8e
Jonathan Cliby <jon.cliby@gmail.com>
2020-04-15 15:18:37 -0500

    set up express

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 // Setup empty JS object to act as endpoint for all routes
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
-/* Middleware*/
+const bodyParser = require('body-parser');
 
+/* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('public'));
 
 // Spin up the server
 // Callback to debug
@@ -19,4 +28,3 @@
 // Callback function to complete GET '/all'
 
 // Post Route
-  
