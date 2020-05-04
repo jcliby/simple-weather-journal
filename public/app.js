1caa88b27c3e4b8f18d0f18406cd579c1131d0bb
Jonathan Cliby <jon.cliby@gmail.com>
2020-05-04 17:47:33 -0500

    Create getWeather and saveEntry

--- public/app.js
+++ public/app.js
@@ -1,12 +1,36 @@
 // Personal API Key for OpenWeatherMap API
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const KEY = 'eac96e3d79f654b5ceb5cafe7a537a19';
 
 // Event listener to add function to existing HTML DOM element
 
 /* Function called by event listener */
 
 /* Function to GET Web API Data*/
+const getWeather = async (url, zip, key) => {
+  try {
+    const res = await fetch(`${url}?zip=${zip}&appid=${key}`);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log('Error: ', e);
+  }
+};
 
 /* Function to POST data */
-
+const saveEntry = async (url = '', data = {}) => {
+  try {
+    await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+  } catch (e) {
+    console.log('Error: ', e);
+  }
+};
 
 /* Function to GET Project Data */
--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 // Setup empty JS object to act as endpoint for all routes
+let posts = [];
+
 // Express to run server and routes
 const express = require('express');
 
@@ -31,5 +33,62 @@ function listening() {
 // Initialize all route with a callback function
 
 // Callback function to complete GET '/all'
+app.get('/all', function (req, res) {
+  res.status(200).send(JSON.stringify(posts));
+});
 
 // Post Route
+app.post('/entry', function (req, res) {
+  console.log(req);
+  let entry = new Entry(
+    generateId(),
+    new Date(),
+    req.body.city,
+    req.body.state,
+    req.body.temperature,
+    req.body.info,
+    req.body.feelings
+  );
+  posts.push(entry);
+  res.status(201).send(JSON.stringify(entry));
+});
+
+function generateId() {
+  return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+// Temp Data
+class Entry {
+  constructor(id, date, city, state, temperature, info, feelings) {
+    this.id = id;
+    this.date = date;
+    this.city = city;
+    this.state = state;
+    this.temperature = temperature;
+    this.info = info;
+    this.feelings = feelings;
+  }
+}
+
+const e1 = new Entry(
+  generateId(),
+  new Date(),
+  'City',
+  'ST',
+  10,
+  'Partly cloudy.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+);
+
+const e2 = new Entry(
+  generateId(),
+  '2 April 2020',
+  new Date(),
+  'ST',
+  10,
+  'Partly cloudy.',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'
+);
+
+posts.push(e1);
+posts.push(e2);
